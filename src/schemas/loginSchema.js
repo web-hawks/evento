b3f2c41a1f9189b1f101c6013699bac060ae5c6d
Menna Allah Yasser <128924490+MennaAllahYasser11@users.noreply.github.com>
2025-01-09 01:36:28 +0200

    feat(EMS-11): Edit validation for password

--- src/schemas/loginSchema.js
+++ src/schemas/loginSchema.js
@@ -1,14 +1,6 @@
-
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-    email: z.string()
-        .nonempty("Email is required")
-        .email("Invalid email format"),
-    password: z.string()
-        .min(8, "Password must be at least 8 characters long")
-        .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-        .regex(/[a-z]/, "Password must contain at least one lowercase letter")
-        .regex(/\d/, "Password must contain at least one number")
-        .regex(/[!@#$%^&*()]/, "Password must contain at least one special character"),
+  email: z.string().nonempty('Email is required').email('Invalid email format'),
+  password: z.string().min(1, 'Password is required'),
 });
